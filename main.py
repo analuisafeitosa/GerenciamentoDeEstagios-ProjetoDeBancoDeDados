0a1d1a26bf8fe664427975bbb29d547cce7fde46
analuisafeitosa <analuisafguimaraes@hotmail.com>
2025-11-19 18:31:29 -0300

    add: mudanças na main. alteração na API para tentar mostrar os dados da nossa tabela pelo VS e não so no PG admin

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import psycopg2
 
-conn = psycopg2.connect(host = "localhost", port = 8888, dbname ="gerenciamento_estagios", user="admin", password="admin123")
-cursor = conn.cursor()
-result = cursor.fetchall()
-print(result)
-cursor.close()
+conn = psycopg2.connect(host = "127.0.0.1", port = 8888, dbname ="gerenciamento_estagios", user="admin", password="admin123")
+
+cur = conn.cursor()
+cur.execute("SELECT current_user, current_database")
+print("Sessão", cur.fetchall())
+
+cur.execute("SELECT * FROM pessoa;")
+print("Dados pessoas", cur.fetchall())
+
+cur.close()
 conn.close()
